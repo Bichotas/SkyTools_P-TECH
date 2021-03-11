370d59312c3af1bd963b4a74408b5f283b6a04c5
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-11 15:05:28 -0600

    Se cambios el nombre

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 #Este va a ser el archivo para ejecutar la página
-from flask import Flask 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('barraNavegación.html')
+
+
+if __name__== '__main__':
+    app.run()
