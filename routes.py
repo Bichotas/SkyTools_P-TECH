2e176ee139710ef1f1513388f1f5616d7439c05a
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-13 16:32:19 -0600

    Archivo en el cual estan/estaran las rutas para las paginas

---
+++ routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import Flask, render_template
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
