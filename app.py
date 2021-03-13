1fd2995e472d1868d886c66f55cd26281259ed54
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-13 10:17:59 -0600

    Se agrego archivo login
    Se utilizo el archivo del register y solo se bajaron algunas cosas en la
    parte del contenedor y los inputs que habia.

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+
 @app.route('/registrarse')
-def index():
-    return render_template('index.html')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__== '__main__':
     app.run(debug=True)
