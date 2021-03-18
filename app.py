448193175873a0d88ee687ba86bee7d2ee598041
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-18 09:36:49 -0600

    Complicado, no se quiere hacer la base de datos unu

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import LoginForm, RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
-import os
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='5d0a9f096785838c292e4212b9deba2a'
@@ -37,5 +35,9 @@ def login():
 def about():
     return render_template('about.html')
 
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 if __name__== '__main__':
     app.run(debug=True)
