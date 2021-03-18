4ac8aa4b8db92ea4950e4a3536583d73a8c93732
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-18 09:20:10 -0600

    Se volvera a hacer las basees de datos

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import LoginForm, RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='5d0a9f096785838c292e4212b9deba2a'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-db.create_all()
 @app.route('/')
 def main():
     return render_template('blank.html')
