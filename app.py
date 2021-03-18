61abaa91f59b82aaf40b73404a5b80f3e66e6211
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-18 16:50:45 -0600

    Se cambio de linea nada mas

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
 
-    __tablename__ = 'roles'
+    __tablename__ = 'Cuentas'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_profile = db.Column(db.String(20), unique=False, default='default.jpg')
     password = db.Column(db.String(60), unique=False, nullable=False)
+    image_profile = db.Column(db.String(20), unique=False, default='default.jpg')
 
 
     def __repr__(self):
