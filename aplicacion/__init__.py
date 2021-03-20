6022a6a137111f98a81b230a26f6f325c6e3bce2
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-20 07:38:53 -0600

    Se tenia una version anterior a la que se queria

--- aplicacion/__init__.py
+++ aplicacion/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -9,5 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from aplicacion import routes
