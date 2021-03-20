f7422613558819696061eb27b42c2f8331ec7e87
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-19 19:45:26 -0600

    Se agregaron algunos paquetes nuevos

--- aplicacion/__init__.py
+++ aplicacion/__init__.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from aplicacion import routes
