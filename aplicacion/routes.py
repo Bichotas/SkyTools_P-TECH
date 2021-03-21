b339bbcf065d6de4a7243e91bb6f48d2cd486e28
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-20 18:18:07 -0600

    Se agrego ruta para actualizar los datos/foto

--- aplicacion/routes.py
+++ aplicacion/routes.py
@@ -70,6 +70,7 @@ def save_picture(form_picture):
 @login_required
 def account():
     form = UpdatingAccountForm()
+    """
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
@@ -84,10 +85,35 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
+        """
     image_file = url_for('static', filename='profile_pics/'+ current_user.image_profile)
     return render_template('profile.html', image_file=image_file, form=form)
 
 
+# Ruta para actualizar datos 
+@app.route('/update', methods=['GET', 'POST'])
+@login_required
+def update():
+    form = UpdatingAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_profile = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been udpated', 'success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/'+ current_user.image_profile)
+    return render_template('update.html', image_file=image_file, form=form)
+
+
+
+
 @app.route('/prueba')
 def prueba():
-    return render_template('account.html')
+    return render_template('blank')
