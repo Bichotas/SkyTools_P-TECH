360771d6dc4ed7de9bebba5c770f28f3f27a84bf
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-03-22 16:09:25 -0600

    Se agregaron rutas y modifico la ruta /

--- aplicacion/routes.py
+++ aplicacion/routes.py
@@ -1,14 +1,15 @@
 import secrets
 import os
 from flask import render_template, url_for, flash, redirect, request
-from aplicacion.models import User
+from aplicacion.models import User, BarraHerramientas
 from aplicacion.forms import LoginForm, RegistrationForm, UpdatingAccountForm
 from aplicacion import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
-def main():
-    return render_template('blank.html')
+def index():
+    incomplete = BarraHerramientas.query.filter_by(complete=False).all()
+    return render_template('blank.html', incomplete=incomplete)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -70,22 +71,6 @@ def save_picture(form_picture):
 @login_required
 def account():
     form = UpdatingAccountForm()
-    """
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_profile = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been udpated', 'success')
-
-        return redirect(url_for('account'))
-
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-        """
     image_file = url_for('static', filename='profile_pics/'+ current_user.image_profile)
     return render_template('profile.html', image_file=image_file, form=form)
 
@@ -113,7 +98,21 @@ def update():
 
 
 
+# Rutas para barra de herramientas
+@app.route('/add', methods=['POST'])
+def add():
 
-@app.route('/prueba')
-def prueba():
-    return render_template('blank')
+    barraHerramientas = BarraHerramientas(text=request.form[barraherra], complete=False)
+    db.session.add(barraHerramientas)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+
+    barraHerramientas = BarraHerramientas.query.filter_by(id=int(id)).first()
+    db.session.query(BarraHerramientas).filter(BarraHerramientas.id==id).delete()
+    db.session.commit()
+
+    return redirect(url_for('index'))
