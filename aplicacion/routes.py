33abcfe030e8469fb8443b1dcf02eb89b93ecf45
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-04-22 09:32:22 -0500

    Pues xd
    Nose aun no se si hacer totalmente la funcion clear ya que no se como hacer que cuando devuelva lo que serian las actvidades del usuario, estas sean eliminadas de la base de datos, ya que esta funcion lo que hace es limpiar todas las actividades de la barra de actividades para que queden en blanco y se puedan agregar nuevas
    
    Probando la funcion Co-outhors
    -Csar
    
    Co-Authored-By: Dromos <79943218+Dromos@users.noreply.github.com>

--- aplicacion/routes.py
+++ aplicacion/routes.py
@@ -1,3 +1,4 @@
+from re import A
 import secrets
 import os
 
@@ -173,12 +174,6 @@ def delete(id):
 def clear():
 
     form = ActividadesInput()
-
-    #hacer una consulta y que lo que se envie, se borre en la base de datos
-    #db.session.query(Activity).filter(Activity.users_id==curr_User).delete()
-    #activities = Activity.query.filter_by(users_id=id_user).all()
-    id_user = current_user.get_id()
-    cl_ear = Activity.query.filter_by(users_id=id_user).delete()
     follana = g.lista_dou 
     return redirect(url_for(follana[0]))
     
