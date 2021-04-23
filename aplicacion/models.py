b669618af1f1adce6842598d5d469eadc8e7f73d
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-04-23 11:53:48 -0500

    Se comento la parte de cateogoria de imagen
    Esta era para poner las imagenes en los cuadros, pero hasta que sea
    necesario, va a ser activada aunque ya este en la base de datso

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -39,4 +39,4 @@ class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(20), unique=True, nullable=False)
-    image_category = db.Column(db.String(30), unique=False, nullable=True)
+    #image_category = db.Column(db.String(30), unique=False, nullable=True)
